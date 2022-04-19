0b93cd7fe6883eaf403617d6077cd3e04891ff86
glitchkn1ght <54705224+glitchkn1ght@users.noreply.github.com>
2022-04-19 15:24:15 +0100

    Added Pokemon model

---
+++ ShakespeareanPokemonAPI/Models/Pokemon.cs
@@ -0,0 +1,18 @@
+﻿//Change History
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// 19/04/2022 Ticket1 JS Team darkSaber - Initial version. 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace ShakespeareanPokemonAPI.Models
+{
+    using Newtonsoft.Json;
+
+    public class Pokemon
+    {
+        [JsonProperty("name")]
+        string Name { get; set; }
+
+        [JsonProperty("description")]
+        string Description { get; set; }
+    }
+}
